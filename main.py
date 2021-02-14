e7ce6d5b72628a2927f2fa4d5328f50369058acd
Jamie Bamforth <jamie_bamforth@hotmail.co.uk>
2021-02-14 20:56:13 +0000

    Updated main so still stores in csv if throttled. This needs more testing, but should work.

--- main.py
+++ main.py
@@ -1,29 +1,44 @@
 from Goodreads_book_scraper import book_scraper
 from Goodreads_list_scraper import list_scraper
-from Proxy_generator_http import proxy_generator_http
+from Proxy_generator import proxy_generator
+from Class_book_record import Book_Record
 import time
 import random
 import csv
 
 ANTI_THROTTLE_DELAY = 10
 
+
+def write_to_csv(book_data):
+    keys = book_data[0].__dict__.keys()
+    print(keys)
+    with open('book_data.csv', 'w', newline='') as output_file:
+        dict_writer = csv.DictWriter(output_file, keys)
+        dict_writer.writeheader()
+        for book in range(len(book_data)):
+            dict_writer.writerow(book_data[book].__dict__)
+
+
 def main():
     start_time = time.time()
     # get proxy addresses to use at random to prevent throttling. Use randomly rather than as an iterator as
     # next(iterator) will eventually run out and have to be re-instantiated.
-    proxy_addresses = proxy_generator_http() # TODO: replace with list of headers?
+    proxy_addresses = proxy_generator()
 
     book_ID_list = list_scraper('http://www.goodreads.com/book/popular_by_date/2020/11', random.choice(proxy_addresses))
-    book_data = [book_scraper(book_ID, random.choice(proxy_addresses)) for book_ID in book_ID_list] # TODO: fails on too many requests? add a wait time?
+    book_data = []
+    try:
+        for book_ID in book_ID_list:
+            book_data_dict = book_scraper(book_ID, random.choice(proxy_addresses))
+            book_data.append(Book_Record(book_data_dict))
+            print(book_data[-1].__dict__)
+    except ConnectionError as err:
+        print(f'Failure to scrape book. {err}')
     print(book_data)
     time_taken = round(time.time() - start_time, 2)
-    print(f'Took {time_taken}s to scrape {len(book_data)} books, {round(time_taken/len(book_data),2)}s per book.')
-
-    keys = book_data[0].keys()
-    with open('book_data.csv', 'w', newline='') as output_file:
-        dict_writer = csv.DictWriter(output_file, keys)
-        dict_writer.writeheader()
-        dict_writer.writerows(book_data)
+    if len(book_data) != 0:
+        print(f'Took {time_taken}s to scrape {len(book_data)} books, {round(time_taken/len(book_data),2)}s per book.')
+        write_to_csv(book_data)
 
 
 if __name__ == '__main__':
