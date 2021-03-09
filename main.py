0d6b4a2ebd75e8f2a8ec2e0332833b20b341ad0d
Jordan Ribbans <jordan.ribbans@live.co.uk>
2021-03-09 14:11:14 +0000

    Added logging, removed config variables, and added argparser

--- main.py
+++ main.py
@@ -2,41 +2,75 @@ from Goodreads_book_scraper import book_scraper
 from Goodreads_list_scraper import list_scraper
 from Class_book_record import Book_Record
 from SQL_uploader_JB import update_db
+from config import *
+import datetime as dt
 import time
 import csv
+import argparse
+import logging
 import sys
 
+logger = logging.getLogger('main')
+logger.setLevel(logging.DEBUG)
 
-OUTPUT_FILE_NAME = 'test_book_data.csv'
-BASE_URL = "https://www.goodreads.com/"
-TYPE_ARG = 1
-DETAILS_ARG = 2
-YEAR_INDEX = 4
-COUNTRY_INDEX = 2
-TYPE = ""
-DETAILS = ""
+# Create Formatter
+formatter = logging.Formatter('%(asctime)s-%(levelname)s-FILE:%(filename)s-FUNC:%(funcName)s-LINE:%(lineno)d-%(message)s')
 
-def list_url():
+# create a file handler and add it to logger
+file_handler = logging.FileHandler('main.log')
+file_handler.setLevel(logging.DEBUG)
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
+
+stream_handler = logging.StreamHandler(sys.stdout)
+stream_handler.setLevel(logging.WARNING)
+stream_handler.setFormatter(formatter)
+logger.addHandler(stream_handler)
+
+
+def args_dict():
+    """
+    Creates a dictionary for the command line arguments
+    The first parameter is the list type, and the second parameter are the list details
+    """
+    type_list = ['most-popular', 'most-read', 'new-releases', 'custom-list']
+    detail_list = ['YYYYMM', 'COUNTRYperiod(eg ILm)', 'genre', 'customID']
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('list_type', choices=type_list, help=f"The type of list to scrape: {type_list}")
+    parser.add_argument('list_detail', help=f"The details for the corresponding list type: {detail_list}")
+
+    logger.debug("Parsing command line arguments")
+    args = parser.parse_args()
+
+    if len(vars(args)) == 2:
+        l_type = args.list_type
+        l_details = args.list_detail
+        logger.debug("List type and detail given")
+    else:
+        logger.warning("Incorrect parameters given, using default list, 'most read'")
+        l_type = ""
+        l_details = ""
+
+    type_detail = {"list_type": l_type, "list_detail": l_details}
+
+    return type_detail
+
+
+def list_url(l_type, l_detail):
     """ Creates the URL of a Goodreads list based on system arguments.
     Defaults to most read this week in local country if none entered. """
     # TODO: Before next checkpoint add in choices and help statements as in command calculator exercise
     # TODO: Before next checkpoint add in errors for incorrectly entered arguments
-    if len(sys.argv) == 3:
-        # <type in [most-popular, most-read, new-releases, custom-list]>
-        # <details in [YYYYMM, COUNTRYperiod(eg ILm), genre, customID]>
-        type = sys.argv[TYPE_ARG]
-        details = sys.argv[DETAILS_ARG]
-    else:
-        type = TYPE
-        details = DETAILS
-    if type == "most-popular":
-        return BASE_URL + "book/popular_by_date/" + str(details)[:YEAR_INDEX] + "/" + str(details)[YEAR_INDEX:]
-    if type == "most-read":
-        return BASE_URL + "book/most_read?category=all&country=" + str(details)[:COUNTRY_INDEX] + "&duration=" + str(details)[-1]
-    if type == "new-releases":
-        return BASE_URL + "genres/new_releases/" + str(details)
-    if type == "custom-list":
-        return BASE_URL + "list/show/" + str(details)
+
+    if l_type == "most-popular":
+        return BASE_URL + "book/popular_by_date/" + str(l_detail)[:YEAR_INDEX] + "/" + str(l_detail)[YEAR_INDEX:]
+    if l_type == "most-read":
+        return BASE_URL + "book/most_read?category=all&country=" + str(l_detail)[:COUNTRY_INDEX] + "&duration=" + str(l_detail)[-1]
+    if l_type == "new-releases":
+        return BASE_URL + "genres/new_releases/" + str(l_detail)
+    if l_type == "custom-list":
+        return BASE_URL + "list/show/" + str(l_detail)
     else:
         return BASE_URL + "book/most_read"
 
@@ -49,41 +83,46 @@ def scrape_books_from_list(book_ID_list):
         for book_ID in book_ID_list:
             book_data_dict = book_scraper(book_ID)
             book_data.append(Book_Record(book_data_dict))
-            print(book_data[-1].__dict__)
+            # print(book_data[-1].__dict__)
     except ConnectionError as err:
-        print(f'Failure to scrape book. {err}')
+        logger.error(f'Failure to scrape book. {err}')
     return book_data
 
 
 def write_to_csv(book_data):
     """Take a list of Book_Record objects and save each Book_record as a row in a CSV."""
-    print(f'Saving scraped data in {OUTPUT_FILE_NAME}.')
+    logger.debug(f'Saving scraped data in {OUTPUT_FILE_NAME}.')
     keys = book_data[0].__dict__.keys()
     with open(OUTPUT_FILE_NAME, 'w', newline='', encoding='utf-8') as output_file:
         dict_writer = csv.DictWriter(output_file, keys)
         dict_writer.writeheader()
         for book in range(len(book_data)):
             dict_writer.writerow(book_data[book].__dict__)
-    print(f'Saved scraped data in {OUTPUT_FILE_NAME}.')
+    logger.info(f'Saved scraped data in {OUTPUT_FILE_NAME}.')
 
 
 def main():
-    """Scrape the given Goodreads list for the book IDs, then scrape each book in the list for it's data. Save that
+    """Scrape the given Goodreads list for the book IDs, then scrape each book in the list for its data. Save that
     data as a CSV."""
     start_time = time.time()
+    logger.info(f"Program started at {dt.datetime.now()}")
 
     # book_ID_list = list_scraper('http://www.goodreads.com/book/popular_by_date/2020/11')
 
-    goodreads_url = list_url()
+    command_dict = args_dict()
+    list_type = command_dict["list_type"]
+    list_detail = command_dict["list_detail"]
+    goodreads_url = list_url(list_type, list_detail)
+
     book_ID_list = list_scraper(goodreads_url)
 
     book_data = scrape_books_from_list(book_ID_list)
 
     time_taken = round(time.time() - start_time, 2)
     if len(book_data) != 0:
-        print(f'Took {time_taken}s to scrape {len(book_data)} books, {round(time_taken/len(book_data),2)}s per book.')
+        logger.info(f'Took {time_taken}s to scrape {len(book_data)} books, {round(time_taken/len(book_data),2)}s per book.')
         # write_to_csv(book_data)
-        update_db(book_data, goodreads_url, 'test_list', 'test_details') #TODO: get type and details args
+        update_db(book_data, goodreads_url, list_type, list_detail) #TODO: get type and details args
 
 
 if __name__ == '__main__':
