9bf8604354c2d0e9f5a9f841b80a5aaaac1b1652
Jordan Ribbans <jordan.ribbans@live.co.uk>
2021-03-09 22:28:17 +0000

    Added config details from book_scraper to config file

--- config.py
+++ config.py
@@ -1,9 +1,15 @@
 
-OUTPUT_FILE_NAME = 'test_book_data.csv' # TODO: remove this as redundant now we have the db
 BASE_URL = "https://www.goodreads.com/"
 YEAR_INDEX = 4
 COUNTRY_INDEX = 2
 TYPE = ""
 DETAILS = ""
-USER = "root"
-PASSWORD = "root" # TODO: remove credentials as user input now taken in cmd line
+ANTI_THROTTLE_DELAY_S = 10
+RELEASE_DATE_ELEMENT_ROW = 1
+RELEASE_DATE_TEXT_LINE = 2
+PAGES_WORD_INDEX_IN_TEXT = 0
+THROTTLING_STATUS_CODE = 403
+OUTPUT_FILE_NAME = "scrape_data.csv"
+
+# Constant url root to which the unique Goodreads ID of a book is appended to access the Goodreads page for that book
+ROOT_BOOK_URL = "http://www.goodreads.com/book/show/"
