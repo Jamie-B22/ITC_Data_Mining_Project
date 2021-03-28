d75610119ec1aad53526245fb1fe1384596c22ee
Jamie Bamforth <jamie_bamforth@hotmail.co.uk>
2021-03-28 19:30:59 +0100

    Class_NYTimes_List.py commented

--- APIs/Class_NYTimes_List.py
+++ APIs/Class_NYTimes_List.py
@@ -11,9 +11,43 @@ import json
 from config import *
 
 
-
 class NYTimesBookList:
+    """
+    Class for storing getting and storing data from the NYT bestsellers lists API:
+        list_name : str                 bestsellers list name
+        list_name_encoded : str         bestsellers list name encoded for use in the API query string
+        date : str (format YYYY-MM-DD)  date on which the list was published
+        list :  list of dicts           a list of the details of the books on the bestseller list for that date
 
+
+        Each book dictionary in the list consists of the following items:
+            rank : integer
+            rank_last_week : integer
+            weeks_on_list : integer
+            asterisk : integer
+            dagger : integer
+            primary_isbn10 : string
+            primary_isbn13 : string
+            publisher : string
+            description : string
+            price : integer
+            title : string
+            author : string
+            contributor : string
+            contributor_note : string
+            book_image : string
+            amazon_product_url : string
+            age_group : string
+            book_review_link : string
+            first_chapter_link : string
+            sunday_review_link : string
+            article_chapter_link : string
+            isbns : array object
+                isbn10 : string
+                isbn13 : string
+
+    Author: Jamie Bamforth
+    """
     def __init__(self, list_name_encoded, date, API_key):  # TODO: raise API key or date errors
         """Takes a list name and a date it was published on (accepts YYYY-MM-DD or "current") and creates a
         NYTimesBookList instance. Possible list name values can be found using static method
@@ -29,71 +63,50 @@ class NYTimesBookList:
                 f'Encoded list name given ({list_name_encoded}) does not exist in the NYT bestsellers list options.')
 
     def _get_list_json(self, list_name, date, API_key):
+        """Takes the arguments fed into the initiation of the object and returns the 'results' key from the json
+        returned by the API GET request."""
         date = '/' + date
         list_name = '/' + list_name
         url = NYT_API_BASE_URL + date + list_name + NYT_API_END_URL + API_key
         return json.loads(requests.get(url).text)['results']
 
     def get_titles(self):
+        """Method to return a list of the titles of the books in the NYTimesBookList instance."""
         return [book['title'] for book in self.list]
 
     def get_authors(self):
+        """Method to return a list of the authors of the books in the NYTimesBookList instance."""
         return [book['author'] for book in self.list]
 
     def get_isbn10s(self):
+        """Method to return a list of the ISBN 10s of the books in the NYTimesBookList instance."""
         return [book['primary_isbn10'] for book in self.list]
 
     def get_isbn13s(self):
+        """Method to return a list of the ISBN 13s of the books in the NYTimesBookList instance."""
         return [book['primary_isbn13'] for book in self.list]
 
     @staticmethod
     def get_list_names(API_key):
+        """Static method to return a list of all the names of all the bestseller lists."""
         url = NYT_API_BASE_URL + '/names' + NYT_API_END_URL + API_key
         list_names = json.loads(requests.get(url).text)
         return [name['list_name'] for name in list_names['results']]
 
     @staticmethod
     def get_list_names_encoded(API_key):
+        """Static method to return a list of all the encoded names of all the bestseller lists. These encoded names can
+        all be used as an input to instantiate a NYTimesBookList object"""
         url = NYT_API_BASE_URL + '/names' + NYT_API_END_URL + API_key
         list_names = json.loads(requests.get(url).text)
         return [name['list_name_encoded'] for name in list_names['results']]
-
-    # @staticmethod
-    # def get_current_list(list_name, API_key):
-    #     # if list_name not in self.get_list_names(): # TODO: validate list name
-    #     date = '/current'
-    #     list_name = '/' + list_name.lower().replace(' ','-')
-    #     url = NYT_API_BASE_URL + date + list_name + NYT_API_END_URL + API_key
-    #     book_results = json.loads(requests.get(url).text)['results']['books']
-    #     return book_results
 
     def __str__(self):
         return str(self.__dict__)
 
 
 if __name__ == '__main__':
-    # date = '/current'
-    # list = '/hardcover-fiction'
-    # url = NYT_API_BASE_URL + date + list + NYT_API_END_URL + NYT_API_key
-    # req = requests.get(url)
-    # list = json.loads(req.text)
-    # jsontest = json.dumps(list, indent=3)
-    # with open('jsontest.txt', 'w') as file:
-    #     file.write(jsontest)
-    #
-    # for book in list['results']['books']:
-    #     print(book['rank'], book['title'], book['price'])
-
-    # lists = json.loads(requests.get('https://api.nytimes.com/svc/books/v3/lists/names.json?api-key=gyAYYsc5MUxhVHVQD3AFDQznc084UhQp').text)
-    # list_names = json.dumps(lists, indent=3)
-    # with open('NYT_list_names.txt', 'w') as file:
-    #     file.write(list_names)
-    # names = [name['list_name'] for name in lists['results']]
-    # print(names)
-
-    # print(NYTimesBooks.get_list_names())
-    # print(NYTimesBookList.get_current_list('Hardcover Fiction', NYT_API_key))
-    # print(NYTimesBookList.get_current_list('Combined Print and E-Book Fiction', NYT_API_key))
+    # some examples of outputs of static methods and class instances if this file is run standalone
     list = NYTimesBookList.get_list_names_encoded(NYT_API_KEY)
     print(len(list))
     print(NYTimesBookList(list[0], 'current', NYT_API_KEY))
@@ -101,7 +114,9 @@ if __name__ == '__main__':
     date = '/current'
     list = '/hardcover-fiction'
     url = NYT_API_BASE_URL + date + list + NYT_API_END_URL + NYT_API_KEY
-    lists = json.loads(requests.get('https://api.nytimes.com/svc/books/v3/lists/names.json?api-key=gyAYYsc5MUxhVHVQD3AFDQznc084UhQp').text)['results']
+    lists = json.loads(requests.get(
+        'https://api.nytimes.com/svc/books/v3/lists/names.json?api-key=gyAYYsc5MUxhVHVQD3AFDQznc084UhQp').text)[
+        'results']
 
     list_names = json.dumps(lists, indent=3)
     print(list_names)
