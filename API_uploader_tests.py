8aa63208addc61248aa751c91575a9185617582d
Jamie Bamforth <jamie_bamforth@hotmail.co.uk>
2021-03-28 10:51:27 +0100

    deleted old OL SQL alchemy classes prototype file.

--- API_uploader_tests.py
+++ API_uploader_tests.py
@@ -11,5 +11,8 @@ from APIs.OpenLibrary_API_Search import isbn_search, OpenLibraryBookInstance
 engine = initialise_engine_and_base()
 OL_book = OpenLibraryBookInstance(isbn_search("059035342X")[0])
 
+OL_book2 = OpenLibraryBookInstance(isbn_search("9780747538486")[0])
+
 
 OL_API_update_db(OL_book, engine)
+OL_API_update_db(OL_book2, engine)
