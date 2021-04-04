12ebee3e6abb19a219a3dec0113c2276cf298c73
Jamie Bamforth <jamie_bamforth@hotmail.co.uk>
2021-04-04 21:06:38 +0100

    Wrapped overnight_batch_JR.py in logging

--- overnight_batch_JR.py
+++ overnight_batch_JR.py
@@ -1,4 +1,29 @@
 from ArgParserDir.Argparser_web_scraping import *
+import logging
+import sys
+import time
+import datetime as dt
+
+logger = logging.getLogger('main')
+logger.setLevel(logging.DEBUG)
+
+# Create Formatter
+formatter = logging.Formatter(
+    '%(asctime)s-%(levelname)s-FILE:%(filename)s-FUNC:%(funcName)s-LINE:%(lineno)d-%(message)s')
+
+# create a file handler and add it to logger
+file_handler = logging.FileHandler('main.log')
+file_handler.setLevel(logging.DEBUG)
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
+
+stream_handler = logging.StreamHandler(sys.stdout)
+stream_handler.setLevel(logging.INFO)
+stream_handler.setFormatter(formatter)
+logger.addHandler(stream_handler)
+
+start_time = time.time()
+logger.info(f"Program started at {dt.datetime.now()}")
 
 logger = logging.getLogger('main')
 logger.setLevel(logging.DEBUG)
@@ -169,4 +194,8 @@ for year in range(2019, 2022):
             web_scraper("most-popular", detail)
         except Exception as err:
             logger.error(err)
+
+
+time_taken = round(time.time() - start_time, 2)
+logger.info(f'Took {time_taken}s to run program')
 
